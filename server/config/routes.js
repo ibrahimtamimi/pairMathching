92c677d9ec826d17b6b3a0acabfa98462549a9c6
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-11-06 14:09:50 +0200

    (feat) add route for history & slots

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,8 @@
 var helpers = require('./helpers.js');
-var dataBase = require ('../dataBase/dataBase.js')
 var student = require('../dataBase/student/studentController.js');
+var history = require('../dataBase/history/historyController.js');
+var dayBord = require('../dataBase/dayBord/dayBordController.js');
+//var dataBase = require ('../dataBase/dataBase.js')
 
 module.exports = function (app, express) {
 	//============================================================================
@@ -8,14 +10,23 @@ module.exports = function (app, express) {
 	//============================================================================	
 		app.post('/api/student/addStudent', student.addStudent);
 		app.get('/api/student/getAllStudent', student.getAllStudent);
-		app.get('/api/student/getStudent', student.getStudent);
-		app.get('/api/student/deleteStudent', student.deleteStudent);
+		app.post('/api/student/getStudent', student.getStudent);
+		app.post('/api/student/deleteStudent', student.deleteStudent);
 	
 	//============================================================================
-	/* 								history  								*/
+	/* 									history  								*/
 	//============================================================================	
-		//app.post('/api/dataBase/testConnection', dataBase.testConnection);
+		app.post('/api/history/addStudentHistory', history.addStudentHistory);
+		app.get('/api/history/getAllStudentHistory', history.getAllStudentHistory);
+		app.post('/api/history/deleteStudentHistory', history.deleteStudentHistory);
 	
+	//============================================================================
+	/* 									dayBord  								*/
+	//============================================================================	
+		app.post('/api/dayBord/addSlot', dayBord.addSlot);
+		app.get('/api/dayBord/getAllSlots', dayBord.getAllSlots);
+		app.post('/api/dayBord/deleteSlot', dayBord.deleteSlot);
+
 	//============================================================================
 	/* 								erorr case  								*/
 	//============================================================================	
