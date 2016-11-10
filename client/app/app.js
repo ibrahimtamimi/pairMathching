4f2fe3c5248ff636e1c310e08c05a7a2cb3c61c6
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-11-10 09:19:27 +0200

    (feat) inteface function

--- client/app/app.js
+++ client/app/app.js
@@ -1,62 +1,61 @@
 angular.module('PairMatching',[
-  'PairMatching.servics',
+  'PairMatching.services',
   'PairMatching.student',
   'ngRoute'
 ])
 
-.controller('HeaderController', function ($scope, $window, $location, Student) {
-  
-  var test = function (argument) {
-    console.log(Student.getAllStudent())
-  }
+.controller('HeaderController', function ($scope, $window, $location, Student, DayBord, History) {
+  $scope.allStudent = [];
+  $scope.getedDataStudent = {};
+  $scope.allHistory = [];
+  //=============================================================================
+    $scope.getSudentInfo = function (id) {
+      Student.getStudent(id)
+    .then(function (resp) {
+        $scope.getedDataStudent = resp                
+      })  
+    };
+
+    $scope.getHistory = function () {
+      History.getAllStudentHistory()
+      .then(function (resp) {
+        $scope.allHistory = resp;
+      })
+    };
+
+    $scope.addToHistory = function (driver, navigater) {
+      History.addStudentHistory({student : driver, old_Pair : navigater});
+    } 
+
+    $scope.shuffleStudent = function () {
+      var j, x, i;
+      for (i = $scope.allStdudent.length; i; i--) {
+          j = Math.floor(Math.random() * i);
+          x = $scope.allStdudent[i - 1];
+          $scope.allStdudent[i - 1] = $scope.allStdudent[j];
+          $scope.allStdudent[j] = x;
+      };
+    };
+    
+    $scope.addSlote = function (driver, navigater) {
+      DayBord.addSlot({driver : driver, navigater : navigater});
+    }
+
+
+
+  //=============================================================================
+    $scope.addStudent = function () {
+      $('.infoData').val('');
+      Student.addStudent($scope.studentData);
+    }
+
+    $scope.drawStudent = function () {
+      var x = Student.getAllStudent()
+      		.then(function (resp) {
+      			$scope.allStudent = resp;
+      			console.log( $scope.allStudent);
+      		})
+    }
 
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// .config(function($routeProvider, $httpProvider){
-// 	$routeProvider
-//   // .when('/',{
-//   //   templateUrl:'app/index.html',
-//   //   controller:'headerController'
-//   // })
-//   .otherwise('/')
-  
-// 	$httpProvider.interceptors.push('AttachTokens');
-// })
-
-// .factory('AttachTokens',function($window){
-// 	var attach = {
-//     request: function (object) {
-//       var jwt = $window.localStorage.getItem('com.PairMatching');
-//       if (jwt) {
-//         object.headers['x-access-token'] = jwt;
-//       }
-//       object.headers['Allow-Control-Allow-Origin'] = '*';
-//       return object;
-//     }
-//   };
-//   return attach;
-// })
-// .run(function ($rootScope, $location) {
-//    $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-//   //   // if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-//   //   //   $location.path('/signin');
-//   //   }
-//    });
-// });
